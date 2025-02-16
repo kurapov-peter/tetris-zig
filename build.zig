4b720fc04dc1680f5dda8a8a7a783736211a928f
Petr Kurapov <petr.kurapov@gmail.com>
2025-02-16 21:17:30 +0100

    Install fonts

--- build.zig
+++ build.zig
@@ -46,6 +46,8 @@ pub fn build(b: *std.Build) void {
     // step when running `zig build`).
     b.installArtifact(exe);
 
+    b.installFile("assets/font.ttf", "assets/font.ttf");
+
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
